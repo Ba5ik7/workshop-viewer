782d6aa54419044824d86510f8900d2ce92f6200
Wesley Dusell <dusellfam@gmail.com>
2022-05-20 16:23:29 -0400

    Add return types to theme picker service methods.

--- src/app/shared/components/theme-picker/theme-picker.service.ts
+++ src/app/shared/components/theme-picker/theme-picker.service.ts
@@ -4,28 +4,30 @@ import { Inject, Injectable } from '@angular/core';
 @Injectable()
 export class ThemePickerService {
 
-  constructor(@Inject(DOCUMENT) private document: Document,) { }
+  constructor(@Inject(DOCUMENT) private document: Document) { }
 
-  setStyle(key: string, href: string) {
+
+
+  setStyle(key: string, href: string): void {
     this._getLinkElementForKey(key).setAttribute('href', href);
   }
 
-  removeStyle(key: string) {
+  removeStyle(key: string): void {
     const existingLinkElement = this._getExistingLinkElementByKey(key);
     if (existingLinkElement) {
       this.document.head.removeChild(existingLinkElement);
     }
   }
 
-  _getLinkElementForKey(key: string) {
+  _getLinkElementForKey(key: string): HTMLLinkElement {
     return this._getExistingLinkElementByKey(key) || this._createLinkElementWithKey(key);
   }
   
-  _getExistingLinkElementByKey(key: string) {
+  _getExistingLinkElementByKey(key: string): HTMLLinkElement | null {
     return this.document.head.querySelector(`link[rel="stylesheet"].style-manager-${key}`);
   }
   
-  _createLinkElementWithKey(key: string) {
+  _createLinkElementWithKey(key: string): HTMLLinkElement {
     const linkEl = this.document.createElement('link');
     linkEl.setAttribute('rel', 'stylesheet');
     linkEl.classList.add(`style-manager-${key}`);
