52dca5d779e8020b1e55383586e5e3239ec1869a
Wesley Dusell <dusellfam@gmail.com>
2022-05-20 03:47:15 -0400

    Add content to 404 component.

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.scss']
 })
-export class NotFoundComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class NotFoundComponent {
+  @HostBinding('class.main-content') readonly mainContentClass = true;
 }
