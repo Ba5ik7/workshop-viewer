ef62007dd1829527204814258732c013886db3b6
Wesley Dusell <dusellfam@gmail.com>
2022-05-20 19:57:40 -0400

    Add Workshop logo

--- src/app/shared/components/navbar/navbar.module.ts
+++ src/app/shared/components/navbar/navbar.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar.component';
 import { ThemePickerModule } from '../theme-picker/theme-picker.module';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   imports: [
     CommonModule,
-    ThemePickerModule
+    ThemePickerModule,
+    MatButtonModule,
+    MatIconModule
   ],
   declarations: [NavbarComponent],
   exports: [NavbarComponent]
