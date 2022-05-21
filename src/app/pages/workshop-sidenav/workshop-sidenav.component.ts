e205c3939cc3e8cb6bec7e5ed2a5a9315ccf85bb
Wesley DuSell <dusellfam@gmail.com>
2022-05-21 18:43:58 -0400

    Watch router for param updates and blind values to the view.

--- src/app/pages/workshop-sidenav/workshop-sidenav.component.ts
+++ src/app/pages/workshop-sidenav/workshop-sidenav.component.ts
@@ -1,6 +1,7 @@
 import { BreakpointObserver } from '@angular/cdk/layout';
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { map, Observable } from 'rxjs';
+import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute, ActivatedRouteSnapshot, ParamMap } from '@angular/router';
+import { combineLatest, forkJoin, map, Observable, of, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'workshop-sidenav',
@@ -8,16 +9,31 @@ import { map, Observable } from 'rxjs';
   styleUrls: ['./workshop-sidenav.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class WorkshopSidenavComponent implements OnInit {
+export class WorkshopSidenavComponent implements OnInit, OnDestroy {
 
   isScreenSmall: Observable<boolean>;
+  destory: Subject<boolean> = new Subject();
 
-  constructor(breakpoints: BreakpointObserver) {
+  sectionTitle!: string;
+  idTitle!: string;
+
+  constructor(breakpoints: BreakpointObserver, private activatedRoute: ActivatedRoute) {
     this.isScreenSmall = breakpoints.observe(`(max-width: 959px)`)
     .pipe(map(breakpoint => breakpoint.matches));
+
+    this.activatedRoute.params
+    .pipe(takeUntil(this.destory))
+    .subscribe((params) => {
+      this.sectionTitle = params['section'] ?? 'ERROR';
+      this.idTitle = params['id'] ?? 'categories';
+    });
   }
 
   ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+    this.destory.next(true);
   }
 
 }
