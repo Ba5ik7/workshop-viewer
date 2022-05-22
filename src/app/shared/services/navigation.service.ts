126d83e252e1502d59438f6156d5b2e93c5c229e
Wesley DuSell <dusellfam@gmail.com>
2022-05-22 13:49:05 -0400

    Create GET service call to retrieve categories.

--- src/app/shared/services/navigation.service.ts
+++ src/app/shared/services/navigation.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, firstValueFrom, Observable } from 'rxjs';
+import { Category } from '../interfaces/category.interface';
 import { Section } from '../interfaces/section.interface';
 
 @Injectable({
@@ -11,6 +12,9 @@ export class NavigationService {
   sectionsSubject = new BehaviorSubject<{ [key: string]: Section } | undefined>(undefined);
   sections$: Observable<any> = this.sectionsSubject.asObservable();
 
+  categoriesSubject = new BehaviorSubject<{ [key: string]: Category[] } | undefined>(undefined);
+  categories$: Observable<any> = this.categoriesSubject.asObservable();
+
   constructor(private httpClient: HttpClient) { }
 
   initializeAppData(): void {
@@ -20,5 +24,10 @@ export class NavigationService {
   getSections(): void {
     this.httpClient.get<{ [key: string]: Section }>('/api/navigation/sections')
     .subscribe((res) => this.sectionsSubject.next(res));
+  }
+
+  getCategories(): void {
+    this.httpClient.get<{ [key: string]: Category[] }>('/api/navigation/categories')
+    .subscribe((res) => this.categoriesSubject.next(res));
   }
 }
