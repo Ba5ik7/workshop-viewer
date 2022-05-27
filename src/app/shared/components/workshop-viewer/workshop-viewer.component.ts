730084c8e25fcd79615f94b16bbaa4e27eea05eb
Wesley DuSell <dusellfam@gmail.com>
2022-05-27 18:58:05 -0400

    Setup component lifecycle for the workshop viewer.

--- src/app/shared/components/workshop-viewer/workshop-viewer.component.ts
+++ src/app/shared/components/workshop-viewer/workshop-viewer.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'workshop-viewer-terrence-dusell',
   templateUrl: './workshop-viewer.component.html',
   styleUrls: ['./workshop-viewer.component.scss']
 })
-export class WorkshopViewerComponent implements OnInit {
+export class WorkshopViewerComponent implements OnInit, OnDestroy {
+
+  destory: Subject<boolean> = new Subject();
 
   constructor() { }
 
   ngOnInit(): void {
+  
+  }
+
+  ngOnDestroy(): void {
+    this.destory.next(true);
   }
 
 }
