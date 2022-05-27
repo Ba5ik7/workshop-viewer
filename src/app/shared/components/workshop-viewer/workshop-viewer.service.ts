bb3ef4474cef0ca26e140a0d4789f927b6940e1c
Wesley DuSell <dusellfam@gmail.com>
2022-05-27 18:56:10 -0400

    Lay down the frame for fecthing workshop files

--- src/app/shared/components/workshop-viewer/workshop-viewer.service.ts
+++ src/app/shared/components/workshop-viewer/workshop-viewer.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, shareReplay, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WorkshopViewerService {
+  private cache: Record<string, Observable<string>> = {};
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  fetchWorkshop(url: string): Observable<string> {
+    if (this.cache[url]) {
+      return this.cache[url];
+    }
+
+    const stream = this.http.get(url, {responseType: 'text'}).pipe(shareReplay(1));
+    return stream.pipe(tap(() => this.cache[url] = stream));
+  }
 }
