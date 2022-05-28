de64903e902a150a45c3bf3cb8d0ae56b74b24a3
Wesley DuSell <dusellfam@gmail.com>
2022-05-28 01:01:05 -0400

    1st run at displaying the workshop.

--- src/app/shared/components/workshop-viewer/workshop-viewer.component.ts
+++ src/app/shared/components/workshop-viewer/workshop-viewer.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Component, ElementRef, OnDestroy, OnInit, SecurityContext } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Subject, takeUntil } from 'rxjs';
+import { WorkshopViewerService } from './workshop-viewer.service';
 
 @Component({
   selector: 'workshop-viewer-terrence-dusell',
@@ -10,10 +12,25 @@ export class WorkshopViewerComponent implements OnInit, OnDestroy {
 
   destory: Subject<boolean> = new Subject();
 
-  constructor() { }
+  textContent = '';
+
+  constructor(
+    private workshopViewerService: WorkshopViewerService,
+    private elementRef: ElementRef,
+    private domSanitizer: DomSanitizer
+    ) { }
 
   ngOnInit(): void {
-  
+    this.workshopViewerService.fetchWorkshop('/api/workshop/example-document')
+    .pipe(takeUntil(this.destory))
+    .subscribe((data: any) => {
+      console.log('DATA', data);
+      // data = data.replace(/href="#([^"]*)"/g, (_m: string, fragmentUrl: string) => {
+      //   const absoluteUrl = `${location.pathname}#${fragmentUrl}`;
+      //   return `href="${this.domSanitizer.sanitize(SecurityContext.URL, absoluteUrl)}"`;
+      // });
+      this.elementRef.nativeElement.innerHTML = data;
+    });
   }
 
   ngOnDestroy(): void {
