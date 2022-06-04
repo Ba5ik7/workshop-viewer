db96f953c9b4502e758b3e665207c69656355f97
Wesley Dusell <dusellfam@gmail.com>
2022-06-04 08:18:42 -0400

    Create the workshop-live-examples module.

---
+++ projects/workshop-live-examples/src/lib/workshop-live-examples.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-workshop-live-examples',
+  template: `
+    <p>
+      workshop-live-examples works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class WorkshopLiveExamplesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
