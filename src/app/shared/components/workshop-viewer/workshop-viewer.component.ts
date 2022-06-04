7972519fe178f7b42df19d558c226650f872f153
Wesley Dusell <dusellfam@gmail.com>
2022-06-04 09:46:21 -0400

    Setup new DomPortal and ComponentPortal to load live workshop examples.

--- src/app/shared/components/workshop-viewer/workshop-viewer.component.ts
+++ src/app/shared/components/workshop-viewer/workshop-viewer.component.ts
@@ -1,4 +1,14 @@
-import { Component, ElementRef, OnDestroy, OnInit, SecurityContext } from '@angular/core';
+import { ComponentPortal, DomPortalOutlet } from '@angular/cdk/portal';
+import {
+  ApplicationRef,
+  Component,
+  ComponentFactoryResolver,
+  ElementRef,
+  Injector,
+  OnDestroy,
+  OnInit,
+  SecurityContext,
+  ViewContainerRef } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Subject, takeUntil } from 'rxjs';
 import { WorkshopDocument } from '../../interfaces/workshop-document.interface';
@@ -14,6 +24,10 @@ export class WorkshopViewerComponent implements OnInit, OnDestroy {
   destory: Subject<boolean> = new Subject();
 
   constructor(
+    private appRef: ApplicationRef,
+    private injector: Injector,
+    private viewContainerRef: ViewContainerRef,
+    private componentFactoryResolver: ComponentFactoryResolver,
     private workshopViewerService: WorkshopViewerService,
     private elementRef: ElementRef,
     private domSanitizer: DomSanitizer
@@ -25,6 +39,7 @@ export class WorkshopViewerComponent implements OnInit, OnDestroy {
     .subscribe((data) => {
       this.correctUrlPaths(data);
       this.elementRef.nativeElement.innerHTML = data.html;
+      this.loadLiveExamples('workshop-live-example', 'Test');
     });
   }
 
@@ -32,6 +47,15 @@ export class WorkshopViewerComponent implements OnInit, OnDestroy {
     data.html = data.html.replace(/href="#([^"]*)"/g, (_m: string, fragmentUrl: string) => {
       const absoluteUrl = `${location.pathname}#${fragmentUrl}`;
       return `href="${this.domSanitizer.sanitize(SecurityContext.URL, absoluteUrl)}"`;
+    });
+  }
+
+  loadLiveExamples(componentName: string, componentClass: any) {
+    const exampleElements = this.elementRef.nativeElement.querySelectorAll(`[${componentName}]`);
+    [...exampleElements].forEach((element: Element) => {
+      const example = element.getAttribute(componentName);
+      const portalHost = new DomPortalOutlet(element, this.componentFactoryResolver, this.appRef, this.injector);
+      const examplePortal = new ComponentPortal(componentClass, this.viewContainerRef);
     });
   }
 
