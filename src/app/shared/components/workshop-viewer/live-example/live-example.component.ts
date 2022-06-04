62d37b1558aac64f8069d96029868663d28c4246
Wesley Dusell <dusellfam@gmail.com>
2022-06-04 09:50:34 -0400

    Create live example component to host the workshops examples.

---
+++ src/app/shared/components/workshop-viewer/live-example/live-example.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'live-example',
+  templateUrl: './live-example.component.html',
+  styleUrls: ['./live-example.component.scss']
+})
+export class LiveExampleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+}
--- src/app/shared/components/workshop-viewer/workshop-viewer.module.ts
+++ src/app/shared/components/workshop-viewer/workshop-viewer.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WorkshopViewerComponent } from './workshop-viewer.component';
 import { WorkshopViewerService } from './workshop-viewer.service';
+import { LiveExampleComponent } from './live-example/live-example.component';
 
 
 
 @NgModule({
-  declarations: [WorkshopViewerComponent],
+  declarations: [WorkshopViewerComponent, LiveExampleComponent],
   exports: [WorkshopViewerComponent],
   imports: [
     CommonModule,
