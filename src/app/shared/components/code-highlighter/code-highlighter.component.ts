95a83ce96400b2531e3e6267eb216473611a45a5
Wesley Dusell <dusellfam@gmail.com>
2022-06-06 00:11:07 -0400

    Create code highlighter shared component using highlighjs.

---
+++ src/app/shared/components/code-highlighter/code-highlighter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-code-highlighter',
+  templateUrl: './code-highlighter.component.html',
+  styleUrls: ['./code-highlighter.component.scss']
+})
+export class CodeHighlighterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
