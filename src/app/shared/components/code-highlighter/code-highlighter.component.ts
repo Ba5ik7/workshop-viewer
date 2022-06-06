4d05acc6692d625c3e0bcf543c173d8f80487335
Wesley Dusell <dusellfam@gmail.com>
2022-06-06 00:18:30 -0400

    Add highlight js themes and component logic to changes theme.

--- src/app/shared/components/code-highlighter/code-highlighter.component.ts
+++ src/app/shared/components/code-highlighter/code-highlighter.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+import { HighlightLoader } from 'ngx-highlightjs';
 
 @Component({
   selector: 'app-code-highlighter',
   templateUrl: './code-highlighter.component.html',
-  styleUrls: ['./code-highlighter.component.scss']
+  styleUrls: ['./code-highlighter.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CodeHighlighterComponent implements OnInit {
+export class CodeHighlighterComponent {
 
-  constructor() { }
+  @Input() code: string = '';
 
-  ngOnInit(): void {
+  themeMap: Map<string, string> = new Map([
+    ['gradient-dark', 'assets/css/highlight-themes/gradient-dark.css'],
+    ['github-dark', 'assets/css/highlight-themes/github-dark.css'],
+    ['github', 'assets/css/highlight-themes/github.css']
+  ]);
+
+  constructor(private hljsLoader: HighlightLoader) { }
+
+  changeTheme(themeColor: string): void {
+    const themePath = this.themeMap.get(themeColor) ?? 'gradient-dark';
+    this.hljsLoader.setTheme(themePath);
   }
-
 }
