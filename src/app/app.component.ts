b7b03e73a9f13ae774946fcc807e4782f5b5bb5a
Wesley DuSell <dusellfam@gmail.com>
2023-02-09 23:03:14 -0500

    konamiCode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { Event, NavigationEnd, NavigationStart, Router } from '@angular/router';
-import { filter, map, pairwise, startWith } from 'rxjs';
+import { filter, fromEvent, map, Observable, pairwise, scan, startWith } from 'rxjs';
 import { NavigationService } from './shared/services/navigation/navigation.service';
 
 @Component({
@@ -24,7 +24,28 @@ export class AppComponent {
     )
     .subscribe(([fromUrl, toUrl]) => {
         resetScrollPosition();
-    })
+    });
+
+    const konamiCode: number[] = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+
+    const keyup$ = fromEvent<KeyboardEvent>(document, 'keyup');
+
+    const konamiCode$ = keyup$.pipe(
+      map(event => event.keyCode),
+      scan((acc: number[], curr: number) => {
+        acc.push(curr);
+        if (acc.length > 10) {
+          acc.shift();
+        }
+        return acc;
+      }, []),
+      filter(sequence => sequence.join(',') === konamiCode.join(','))
+    );
+    
+    konamiCode$.subscribe(() => {
+      alert("Konami code entered!");
+    });
+
   }
 }
 
